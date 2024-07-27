d01eeae984814c913ed49a8c3f5fde7ff97b5a38
ArthSales <arthur.snascimento01@gmail.com>
2024-07-27 13:00:29 -0300

    Teste com tuplas pra função sacoAzulejos, sacoAzulejos :: define o range de azulejos possíveis para a loja de azulejos, e garante o reset desse range quando os azulejos no saco chegam a zero.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
-module Main (main) where
+module Main (main, Azulejos) where
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Entre com a lista de azulejos::"
+  line <- getLine
+  print (sacoAzulejos [(20,line)])
 
-f :: Int -> Int
-f a = a
+type Azulejos = [(Int,String)] 
+
+sacoAzulejos :: Azulejos -> Azulejos
+sacoAzulejos [] = [(20,"Azul"),(20,"Amarelo"),(20,"Vermelho"),(20,"Preto"),(20,"Branco")]
+sacoAzulejos ((x,y):zs) =  x + sacoAzulejos zs
