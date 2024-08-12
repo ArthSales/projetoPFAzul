9fc9e11b5ef8e57b90e132729fe5663d9681367b
ArthSales <arthur.snascimento01@gmail.com>
2024-08-12 13:59:18 -0300

    review

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,6 @@ main :: IO ()
 
 main = do
    putStrLn "Entre com a lista de azulejos::"
-<<<<<<< HEAD
    print (sacoAzulejos [(4,Azul),(10,Amarelo),(10,Vermelho),(6,Preto),(10,Branco)])
    print (sacoAzulejos [(0,Azul),(2,Amarelo),(3,Vermelho),(10,Preto),(5,Branco)])
    let azulejosTeste = azulejosParaNum (sacoAzulejos [(0,Azul),(2,Amarelo),(3,Vermelho),(10,Preto),(5,Branco)]) 0
@@ -33,43 +32,5 @@ main = do
   --  mapM_ print paredeAtualizada
   --  putStrLn "\nPattern Lines atualizadas:"
   --  mapM_ print patternLinesAtualizadas
-=======
-   print (sacoAzulejos [(4,"Azul"),(10,"Amarelo"),(10,"Vermelho"),(6,"Preto"),(10,"Branco")])
-   print (sacoAzulejos [(0,"Azul"),(2,"Amarelo"),(3,"Vermelho"),(10,"Preto"),(5,"Branco")])
-   print (azulejosParaNum (sacoAzulejos [(0,"Azul"),(2,"Amarelo"),(3,"Vermelho"),(10,"Preto"),(5,"Branco")]) 0 )
-
---Renomeia a lista de tuplas [(Int,String)] para definir a lista de azulejos
-type Azulejos = [(Int,String)]
-type AzulejosSeparados = (Int,String)
-type Cor = String
---type Expositores = [[String]]
-
---sacoAzulejos: define o saco de azulejos do jogo Azul, que contém 20 peças de cada uma das cinco cores e garante que caso
---a soma das peças seja menor que 20, o saco volta a ter as 100 peças iniciais
-sacoAzulejos :: Azulejos -> Azulejos
-sacoAzulejos [] = [(20,"Azul"),(20,"Amarelo"),(20,"Vermelho"),(20,"Preto"),(20,"Branco")]
-sacoAzulejos xs | totalAzulejos < 20 = [(20,"Azul"),(20,"Amarelo"),(20,"Vermelho"),(20,"Preto"),(20,"Branco")]
-                | otherwise = xs
-                  where totalAzulejos = sum (map fst xs)
-
-somaAzulejo :: Cor -> Azulejos -> Azulejos
-somaAzulejo _ [] = [(20,"Azul"),(20,"Amarelo"),(20,"Vermelho"),(20,"Preto"),(20,"Branco")]
-somaAzulejo cor ((q,c):azulejos) | cor == c = (q+1, c): azulejos
-                                 | otherwise = (q,c) : somaAzulejo cor azulejos
-
-azulejosParaNum :: Azulejos -> Int -> [AzulejosSeparados]
-azulejosParaNum [] _ = []
-azulejosParaNum ((0, _):xs) acc = azulejosParaNum xs acc
-azulejosParaNum (x:xs) acc = (acc,snd x): azulejosParaNum ((fst x - 1, snd x):xs) (acc+1)
-
-numParaAzulejos :: [AzulejosSeparados] -> Azulejos -> Azulejos
-numParaAzulejos [] azulejos = azulejos
-numParaAzulejos ((_,c):azulejosSeparados) azulejos = numParaAzulejos azulejosSeparados (somaAzulejo c azulejos)
-
--- 
---expositores :: [Azulejos] -> Expositores
---expositores [] = []
---expositores azulejos = 
->>>>>>> 700dcf5 (review)
 
 
